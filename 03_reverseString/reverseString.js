d6d553371d3fda20c643393a81cc51fcd3e1852f
hansdf <dornsback@gmail.com>
2024-05-23 19:21:17 -0300

    some attempts

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(word) {
+    charsOfWord = word.split("")
+    let reversedString = ""
 
-};
+    while (charsOfWord.length > 0) {
+        reversedString = reversedString + charsOfWord.pop()
+    }
+
+    return reversedString
+}
 
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let addingUp = 0;
+    while (addingUp < num2) {
+        addingUp = num1 + 1;
+    }
 
+    let finalSum = num1 + addingUp + num2;
+    return finalSum
 };
 
 // Do not edit below this line
